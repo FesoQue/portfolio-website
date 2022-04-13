b9c0275488de7635cf23415352084b47d1c7ed58
FesoQue <adefesoq@gmail.com>
2022-04-13 11:12:34 +0100

    building the nav for my portfolio website

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { FaHome } from 'react-icons/fa';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className='container'>
+        <div className='logo-wrapper'>
+          <div className='logo'></div>
+          <h5>Adebola</h5>
+        </div>
+        <nav>
+          <div className='menu'>
+            <button className='menu-btn'>
+              <span>
+                <FaHome />
+              </span>
+            </button>
+            <button className='menu-btn'>
+              <span>
+                <FaHome />
+              </span>
+            </button>
+            <button className='menu-btn'>
+              <span>
+                <FaHome />
+              </span>
+            </button>
+            <button className='menu-btn'>
+              <span>
+                <FaHome />
+              </span>
+            </button>
+            <button className='menu-btn'>
+              <span>
+                <FaHome />
+              </span>
+            </button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
